96adc004d29dec575abfbd490cc428d1d30ce947
byteom <123omsinghpipariya@gmail.com>
2025-07-08 20:30:15 +0530

    internship page correct

--- src/app/VerifyInternshipPage/page.tsx
+++ src/app/VerifyInternshipPage/page.tsx
@@ -1,6 +1,5 @@
-export const runtime = "edge";
-
 'use client';
+export const runtime = "edge";
 
 import React, { useEffect, useState, useCallback, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
